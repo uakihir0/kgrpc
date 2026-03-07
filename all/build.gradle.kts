af766b4c7f623ec65bc5eefe8d98bb2c18183f35
URUSHIHARA Akihiro <a.urusihara@gmail.com>
2026-03-08 02:28:08 +0900

    Add Apple aggregation module with XCFramework and CocoaPods support
    - all/build.gradle.kts with JVM, JS, iOS, macOS targets
    - XCFramework generation for Apple platforms
    - CocoaPods integration for iOS/macOS distribution
    - Re-exports core module as the public API

---
+++ all/build.gradle.kts
@@ -0,0 +1,69 @@
+import org.gradle.api.tasks.compile.JavaCompile
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.cocoapods)
+    id("module.publications")
+}
+
+kotlin {
+    jvm {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+
+    js(IR) {
+        nodejs()
+        browser()
+
+        binaries.library()
+
+        compilerOptions {
+            target.set("es2015")
+        }
+    }
+
+    val xcf = XCFramework("kgrpc")
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64(),
+        macosX64(),
+        macosArm64(),
+    ).forEach {
+        it.binaries.framework {
+            export(project(":core"))
+            baseName = "kgrpc"
+            xcf.add(this)
+        }
+    }
+
+    cocoapods {
+        name = "kgrpc"
+        version = "0.0.1"
+        summary = "kgrpc is a Kotlin Multiplatform gRPC client library."
+        homepage = "https://github.com/uakihir0/kgrpc"
+        authors = "Akihiro Urushihara"
+        license = "MIT"
+        framework { baseName = "kgrpc" }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            api(project(":core"))
+        }
+    }
+}
+
+tasks.configureEach {
+    if (name.contains("assembleKgrpc") && name.contains("XCFramework")) {
+        mustRunAfter(tasks.matching { it.name.contains("FatFramework") })
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(11)
+}
