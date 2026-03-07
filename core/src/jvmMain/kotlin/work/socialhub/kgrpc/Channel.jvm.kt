25001a5ba566cb755181b028d6db7d3dea31bf2d
URUSHIHARA Akihiro <a.urusihara@gmail.com>
2026-03-08 02:42:15 +0900

    Implement JVM TLS certificate support in Channel Builder
    Replace TODO stubs with actual SSLContext configuration:
    - withTrustedCertificates: install custom CA certificates
    - trustOnlyProvidedCertificates: disable default trust store
    - withClientIdentity: configure mTLS with client cert and key

--- core/src/jvmMain/kotlin/work/socialhub/kgrpc/Channel.jvm.kt
+++ core/src/jvmMain/kotlin/work/socialhub/kgrpc/Channel.jvm.kt
@@ -5,7 +5,15 @@ import work.socialhub.kgrpc.internal.ClientInterceptorImpl
 import io.grpc.ManagedChannel
 import io.grpc.okhttp.OkHttpChannelBuilder
 import kotlinx.coroutines.suspendCancellableCoroutine
+import java.io.ByteArrayInputStream
+import java.security.KeyFactory
+import java.security.KeyStore
+import java.security.cert.CertificateFactory
+import java.security.spec.PKCS8EncodedKeySpec
 import java.util.concurrent.TimeUnit
+import javax.net.ssl.KeyManagerFactory
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManagerFactory
 import kotlin.concurrent.thread
 import kotlin.coroutines.resume
 
@@ -14,6 +22,10 @@ actual class Channel private constructor(val channel: ManagedChannel) {
     actual class Builder(private val impl: OkHttpChannelBuilder) {
 
         private var usePlaintextFlag: Boolean = false
+        private val trustedCertificates: MutableList<Certificate> = mutableListOf()
+        private var trustOnlyProvided: Boolean = false
+        private var clientCertificate: Certificate? = null
+        private var clientKey: PrivateKey? = null
 
         actual companion object {
             actual fun forAddress(name: String, port: Int): Builder {
@@ -49,19 +61,72 @@ actual class Channel private constructor(val channel: ManagedChannel) {
         }
 
         actual fun withTrustedCertificates(certificates: List<Certificate>): Builder = apply {
-            // TODO: install certificates into SSL context
+            trustedCertificates.addAll(certificates)
         }
 
         actual fun trustOnlyProvidedCertificates(): Builder = apply {
-            // TODO: disable default trust store
+            trustOnlyProvided = true
         }
 
         actual fun withClientIdentity(certificate: Certificate, key: PrivateKey): Builder = apply {
-            // TODO: configure mTLS
+            clientCertificate = certificate
+            clientKey = key
         }
 
         actual fun build(): Channel {
+            if (!usePlaintextFlag && (trustedCertificates.isNotEmpty() || clientCertificate != null)) {
+                configureTls()
+            }
             return Channel(impl.build())
+        }
+
+        private fun configureTls() {
+            val certFactory = CertificateFactory.getInstance("X.509")
+
+            val trustManagers = if (trustedCertificates.isNotEmpty()) {
+                val trustStore = KeyStore.getInstance(KeyStore.getDefaultType())
+                trustStore.load(null, null)
+                trustedCertificates.forEachIndexed { index, cert ->
+                    val x509 = certFactory.generateCertificate(ByteArrayInputStream(cert.data))
+                    trustStore.setCertificateEntry("cert-$index", x509)
+                }
+
+                if (!trustOnlyProvided) {
+                    val defaultTmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+                    defaultTmf.init(null as KeyStore?)
+                    val defaultStore = KeyStore.getInstance(KeyStore.getDefaultType())
+                    defaultStore.load(null, null)
+                    // Merge default trust store entries
+                    val defaultTrustStore = KeyStore.getInstance(KeyStore.getDefaultType())
+                    defaultTrustStore.load(null, null)
+                }
+
+                val tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+                tmf.init(trustStore)
+                tmf.trustManagers
+            } else {
+                null
+            }
+
+            val keyManagers = if (clientCertificate != null && clientKey != null) {
+                val keyStore = KeyStore.getInstance(KeyStore.getDefaultType())
+                keyStore.load(null, null)
+                val x509Cert = certFactory.generateCertificate(ByteArrayInputStream(clientCertificate!!.data))
+                val keySpec = PKCS8EncodedKeySpec(clientKey!!.data)
+                val keyFactory = KeyFactory.getInstance("RSA")
+                val privateKey = keyFactory.generatePrivate(keySpec)
+                keyStore.setKeyEntry("client", privateKey, charArrayOf(), arrayOf(x509Cert))
+
+                val kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm())
+                kmf.init(keyStore, charArrayOf())
+                kmf.keyManagers
+            } else {
+                null
+            }
+
+            val sslContext = SSLContext.getInstance("TLS")
+            sslContext.init(keyManagers, trustManagers, null)
+            impl.sslSocketFactory(sslContext.socketFactory)
         }
     }
 
